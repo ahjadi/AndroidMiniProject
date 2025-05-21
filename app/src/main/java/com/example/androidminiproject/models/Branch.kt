8c57e50ad23d27e3f9a841ceec118273018c0d03
ahjadi <alihussainaljadi@gmail.com>
2025-05-21 19:41:48 +0300

    Create a branch model

---
+++ app/src/main/java/com/example/androidminiproject/models/Branch.kt
@@ -0,0 +1,16 @@
+package com.example.androidminiproject.models
+
+data class Branch(
+    val id: Int,
+    val name: String,
+    val type: BranchType,
+    val address: String,
+    val phone: String,
+    val hours: String,
+    val locationURL: String,
+    val imageUri: Int
+
+)
+enum class BranchType{
+    PHYSICAL, VIRTUAL
+}
